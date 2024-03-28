492e0bd535e451efcb98312fcbc16f56ca2112c4
marco.delgamba <marco.delgamba@spindox.it>
2024-03-28 15:49:16 +0100

    new

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServletInitializer.class, args);
+        SpringApplication.run(ServletInitializer.class, args); 
     }
 
     @GetMapping("/")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        
         return String.format("Hello %s!", name);
     }
 
