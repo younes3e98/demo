bc9b68af9ed1d77c2700949333aa85547a0b5c69
younes <younes.elqasby@spindox.it>
2024-03-29 15:18:25 +0100

    Rimosso DemoApplication e aggiunto SpringBootServletInitializer a ServletInitializer

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -3,13 +3,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ServletInitializer {
+public class ServletInitializer extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(ServletInitializer.class, args); 
@@ -17,7 +18,7 @@ public class ServletInitializer {
 
     @GetMapping("/")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        
+
         return String.format("Hello %s!", name);
     }
 
